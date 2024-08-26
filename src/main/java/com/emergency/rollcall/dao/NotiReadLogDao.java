f6bce43ad033c8a2d8f27b1105f233f36c835b6f
khaingzarchi25 <khaingzarchi25494@gmail.com>
2024-08-26 10:43:39 +0630

    commit_edit dashboard count

--- src/main/java/com/emergency/rollcall/dao/NotiReadLogDao.java
+++ src/main/java/com/emergency/rollcall/dao/NotiReadLogDao.java
@@ -13,18 +13,18 @@ import com.emergency.rollcall.entity.NotiReadLog;
 public interface NotiReadLogDao extends JpaRepository<NotiReadLog, Long> {
 
 	@Query(value = "SELECT u.name AS username,u.staffid AS staffid,n.read_noti_date AS notidate, n.read_noti_time AS notitime,ec.name AS ename "
-			+ "FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.id = n.staff_id "
+			+ "FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.staffid = n.staff_id "
 			+ "LEFT JOIN ERC_Emergency_ACTIVATE ec ON ec.syskey = n.emergency_id "
-			+ "WHERE ec.syskey = :emergencyId AND ( LOWER(u.name) LIKE LOWER('%' || :params || '%') OR LOWER(ec.name) LIKE LOWER('%' || :params || '%')) ", countQuery = "SELECT COUNT(*) FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.id = n.staff_id "
+			+ "WHERE ec.syskey = :emergencyId AND ( LOWER(u.name) LIKE LOWER('%' || :params || '%') OR LOWER(ec.name) LIKE LOWER('%' || :params || '%')) ", countQuery = "SELECT COUNT(*) FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.staffid = n.staff_id "
 					+ "LEFT JOIN ERC_Emergency_ACTIVATE ec ON ec.syskey = n.emergency_id "
 					+ "WHERE ec.syskey = :emergencyId AND ( LOWER(u.name) LIKE LOWER('%' || :params || '%')  OR LOWER(ec.name) LIKE LOWER('%' || :params || '%') ) ", nativeQuery = true)
 	Page<Map<String, Object>> findByUserName(Pageable pageable, @Param("params") String params,
 			@Param("emergencyId") Long emergencyId);
 
 	@Query(value = "SELECT u.name AS username,u.staffid AS staffid,n.read_noti_date AS notidate, n.read_noti_time AS notitime,ec.name AS ename "
-			+ " FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.id = n.staff_id "
+			+ " FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.staffid = n.staff_id "
 			+ "LEFT JOIN ERC_Emergency_ACTIVATE ec ON ec.syskey = n.emergency_id "
-			+ "WHERE ec.syskey = :emergencyId ", countQuery = "SELECT COUNT(*) FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.id = n.staff_id "
+			+ "WHERE ec.syskey = :emergencyId ", countQuery = "SELECT COUNT(*) FROM ERC_NOTI_READ_LOG n LEFT JOIN PYM_User u ON u.staffid = n.staff_id "
 					+ "LEFT JOIN ERC_Emergency_ACTIVATE ec ON ec.syskey = n.emergency_id "
 					+ "WHERE ec.syskey = :emergencyId ", nativeQuery = true)
 	Page<Map<String, Object>> findByUserName(Pageable pageable, @Param("emergencyId") Long emergencyId);
