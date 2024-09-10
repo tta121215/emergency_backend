5283af7c13c379ab24f46f714b364a9743bc7b3a
khaingzarchi25 <khaingzarchi25494@gmail.com>
2024-09-10 19:00:02 +0630

    commit_adddto

---
+++ src/main/java/com/emergency/rollcall/dto/HeadCountDto.java
@@ -0,0 +1,117 @@
+package com.emergency.rollcall.dto;
+
+
+public class HeadCountDto {
+	
+	private String email;
+	private String icnumber;
+	private String mobileNo;
+	private String name;
+	private String passportNumber;
+	private String staffId;
+	private String username;
+	private String company;
+	private String country;		
+	private String lastEntryPoint;
+	private String department;
+	private String type;
+
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getIcnumber() {
+		return icnumber;
+	}
+
+	public void setIcnumber(String icnumber) {
+		this.icnumber = icnumber;
+	}
+
+	public String getMobileNo() {
+		return mobileNo;
+	}
+
+	public void setMobileNo(String mobileNo) {
+		this.mobileNo = mobileNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassportNumber() {
+		return passportNumber;
+	}
+
+	public void setPassportNumber(String passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+	public String getStaffId() {
+		return staffId;
+	}
+
+	public void setStaffId(String staffId) {
+		this.staffId = staffId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getLastEntryPoint() {
+		return lastEntryPoint;
+	}
+
+	public void setLastEntryPoint(String lastEntryPoint) {
+		this.lastEntryPoint = lastEntryPoint;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
